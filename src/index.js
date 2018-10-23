87ad00c1be2149a860d2357b254194b8fe9dd14d
Austin Malerba <austin.malerba@gmail.com>
2018-10-22 22:10:55 -0500

    gh-pages and hashrouter

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux'
 import App from 'components/App';
 import * as serviceWorker from './serviceWorker';
 import store from 'store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
